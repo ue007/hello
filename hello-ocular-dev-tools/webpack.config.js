664e928d7f357d9f66887bc14469ef4db688a309
ue007 <82075874+ue007@users.noreply.github.com>
2022-10-10 10:46:46 +0800

    feat: add hello-rollup

--- hello-ocular-dev-tools/webpack.config.js
+++ hello-ocular-dev-tools/webpack.config.js
@@ -1,4 +1,5 @@
 const getWebpackConfig = require('ocular-dev-tools/config/webpack.config');
+console.log(getWebpackConfig);
 module.exports = (env) => {
 	const config = getWebpackConfig(env);
 	// add custom settings
